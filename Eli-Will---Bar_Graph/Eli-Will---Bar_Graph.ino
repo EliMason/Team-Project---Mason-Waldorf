2f6545ae5dc265a44dfd23981bd08ba7737a1001
Eli Mason <02eli.mason@gulllakecs.org>
2019-05-01 12:23:01 -0400

    Add new ino file

---
+++ Eli-Will---Bar_Graph/Eli-Will---Bar_Graph.ino
@@ -0,0 +1,24 @@
+/* 
+5.1.19
+Creators: Eli Mason Will Waldorf
+We are diseinging a project where if we tist a potntiameter a certain way a certain
+amount of lights will light up and if we twist the other way a different amount of lights
+will light up
+ */
+
+
+
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
