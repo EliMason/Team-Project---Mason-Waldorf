31731e263476676ff12340683d47ebdd4265510a
Will Waldorf <02william.waldorf@gulllakecs.org>
2019-05-02 12:47:59 -0400

    Change max number

--- Eli-Will---Bar_Graph/Eli-Will---Bar_Graph.ino
+++ Eli-Will---Bar_Graph/Eli-Will---Bar_Graph.ino
@@ -10,7 +10,7 @@ will light up
  
 // these constants won't change:
 const int analogPin = A0;   // the pin that the potentiometer is attached to
-const int ledCount = 10;    // the number of LEDs in the bar graph
+const int ledCount = 13;    // the number of LEDs in the bar graph
 
 int ledPins[] = {
   4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
