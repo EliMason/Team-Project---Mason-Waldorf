83397e1f8954a756726360ea7b776726e0c24580
Will Waldorf <02william.waldorf@gulllakecs.org>
2019-05-02 12:41:14 -0400

    Change first commets

--- Eli-Will---Bar_Graph/Eli-Will---Bar_Graph.ino
+++ Eli-Will---Bar_Graph/Eli-Will---Bar_Graph.ino
@@ -14,7 +14,7 @@ const int ledCount = 10;    // the number of LEDs in the bar graph
 
 int ledPins[] = {
   4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
-};   // an array of pin numbers to which LEDs are attached
+};   // LED Pin Numbers
 
 
 void setup() {
