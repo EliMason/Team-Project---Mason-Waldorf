e1c9b6ad0a3188b479779ad38cccf356fe511465
Will Waldorf <02william.waldorf@gulllakecs.org>
2019-05-02 12:32:26 -0400

    Change LED Pin Numbers (No 2,3 added 12,13)

--- Eli-Will---Bar_Graph/Eli-Will---Bar_Graph.ino
+++ Eli-Will---Bar_Graph/Eli-Will---Bar_Graph.ino
@@ -13,7 +13,7 @@ const int analogPin = A0;   // the pin that the potentiometer is attached to
 const int ledCount = 10;    // the number of LEDs in the bar graph
 
 int ledPins[] = {
-  2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+  4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
 };   // an array of pin numbers to which LEDs are attached
 
 
